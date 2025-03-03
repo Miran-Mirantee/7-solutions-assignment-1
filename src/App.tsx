ce373f0933baf1655dc92d0ddb31e7da4ca4ce0e
Miran-Mirantee <killerm11805@hotmail.com>
2025-03-03 18:48:10 +0700

    created basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import data from "./data.json";
+
+const Column = ({ header }: { header: string }) => {
+  return (
+    <div className="column">
+      <div className="header">{header}</div>
+      <div className="content">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
     <>
-      <div>testing</div>
+      <div className="container">
+        <div className="list">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <Column header="Fruit" />
+        <Column header="Vegetable" />
+      </div>
     </>
   );
 }
